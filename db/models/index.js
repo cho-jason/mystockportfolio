ab344a4ce2bb03557463b2b5de3083e44270c7ae
Jason Cho <jason.cho.86@gmail.com>
2020-03-05 10:50:58 -0500

    feat(db): Transaction model created

--- db/models/index.js
+++ db/models/index.js
@@ -1,8 +1,11 @@
 const User = require('./user')
 const Stock = require('./stock')
+const Transaction = require('./transaction')
 
 // ASSOCIATIONS
 Stock.belongsTo(User)
+Transaction.belongsTo(User)
 User.hasMany(Stock)
+User.hasMany(Transaction)
 
-module.exports = { User, Stock }
+module.exports = { User, Stock, Transaction }
---
+++ db/models/transaction.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize')
+const db = require('..')
+
+const Transaction = db.define('transaction', {
+  stockSymbol: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  shares: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  // pricePerShare is in pennies to avoid precision errors
+  pricePerShare: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  date: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW,
+    allowNull: false
+  }
+})
+
+// HOOKS
+const capitalizeStockSymbol = transaction => {
+  transaction.stockSymbol = transaction.stockSymbol.toUpperCase()
+}
+
+Transaction.beforeCreate(capitalizeStockSymbol)
+
+module.exports = Transaction
