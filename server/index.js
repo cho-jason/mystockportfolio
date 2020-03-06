8e68eddfa8b5e6f8b68f5886aa4e75412de78817
Jason Cho <jason.cho.86@gmail.com>
2020-03-06 14:37:25 -0500

    build: express-session, passport, connect-session-sequelize added as dependencies

--- server/index.js
+++ server/index.js
@@ -3,17 +3,51 @@ const app = express()
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const path = require('path')
+const session = require('express-session')
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+const passport = require('passport')
 const db = require('../db')
 const PORT = process.env.PORT || 3001
 
 // Load secets if non-production environments
 if (process.env.NODE_ENV !== 'production') require('../secrets')
 
+// Register passport
+passport.serializeUser((user, done) => {
+  try {
+    done(null, user.id)
+  } catch (err) {
+    done(err)
+  }
+})
+passport.deserializeUser(async (userId, done) => {
+  try {
+    const user = await db.models.user.findByPK(userId)
+    done(null, user)
+  } catch (err) {
+    next(err)
+  }
+})
+
 // Middleware
 app.use(morgan('dev'))
 app.use(express.static(path.join(__dirname, '../client/build')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+// Create our database store
+const dbStore = new SequelizeStore({ db })
+// Sync database store so table can be created in db
+dbStore.sync()
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    store: dbStore,
+    resave: false,
+    saveUninitialized: false
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Routes
 app.use('/api', require('./api'))
