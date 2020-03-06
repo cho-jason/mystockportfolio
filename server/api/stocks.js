2b0c37acd6a3253f1be562df037c6bf44e163264
Jason Cho <jason.cho.86@gmail.com>
2020-03-06 17:57:58 -0500

    refactor(api): modified sequence of try / catch blocks for IEX API calls

--- server/api/stocks.js
+++ server/api/stocks.js
@@ -3,26 +3,20 @@ const axios = require('axios')
 
 // GET ROUTES
 router.get('/:symbol', async (req, res, next) => {
+  const { symbol } = req.params
+  const API_KEY = process.env.IEX_API_KEY
+
   try {
-    const { symbol } = req.params
-    const API_KEY = process.env.IEX_API_KEY
-
-    const stock = await axios
-      .get(
-        `https://cloud.iexapis.com/stable/stock/${symbol}/quote?token=${API_KEY}`
-      )
-      .catch(err => {
-        console.log(err.message)
-        console.log(err.stack)
-        res.status(400).send(`Invalid ticker symbol: ${symbol}.`)
-      })
-    const stockData = stock.data
-
-    if (stockData) {
-      res.status(200).json(stockData)
-    }
+    const stock = await axios.get(
+      `https://cloud.iexapis.com/stable/stock/${symbol}/quote?token=${API_KEY}`
+    )
+    res.status(200).json(stock.data)
   } catch (err) {
-    next(err)
+    if (err.response.status === 404) {
+      res.status(404).send(`Invalid ticker symbol: ${symbol}.`)
+    } else {
+      next(err)
+    }
   }
 })
 
--- server/api/transactions.js
+++ server/api/transactions.js
@@ -24,69 +24,63 @@ router.get('/:id', async (req, res, next) => {
 
 // POST ROUTES
 router.post('/', async (req, res, next) => {
+  const symbol = req.body.symbol
+  const shares = parseInt(req.body.shares)
+  const userId = parseInt(req.body.userId)
+  const API_KEY = process.env.IEX_API_KEY
+
   try {
-    // const { symbol, shares, userId } = req.body
-    const symbol = req.body.symbol
-    const shares = parseInt(req.body.shares)
-    const userId = parseInt(req.body.userId)
-    const API_KEY = process.env.IEX_API_KEY
+    const stock = await axios.get(
+      `https://cloud.iexapis.com/stable/stock/${symbol}/quote?token=${API_KEY}`
+    )
+    const stockPrice = parseInt(stock.data.latestPrice)
+    // Note: stockPrice is price per share in dollars
+    const totalCost = shares * stockPrice * 100
+    // Note: user.balance is in pennies
+    let user = await User.findByPk(userId)
 
-    const stock = await axios
-      .get(
-        `https://cloud.iexapis.com/stable/stock/${symbol}/quote?token=${API_KEY}`
-      )
-      .catch(err => {
-        console.log(err.message)
-        console.log(err.stack)
-        res.status(400).send(`Invalid ticker symbol: ${symbol}`)
+    // If user has insufficient funds, notify user of bad request
+    if (user.balance < totalCost) {
+      res.status(400).send('Insufficient balance.')
+
+      // Othewise, purchase x amount of stock shares
+    } else {
+      // 1. Create new transaction
+      const transaction = await Transaction.create({
+        stockSymbol: symbol,
+        pricePerShare: stockPrice * 100,
+        shares
       })
-    const stockData = stock.data
-    if (stockData) {
-      // Note: stockData.latestPrice is in dollars
-      const totalCost = shares * stockData.latestPrice * 100
-      // Note: user.balance is in pennies
-      let user = await User.findByPk(userId)
+      await user.addTransaction(transaction)
 
-      // If user has insufficient balance to buy stock shares,
-      // notify user of bad request
-      if (user.balance < totalCost) {
-        res.status(400).send('Insufficient balance.')
+      // 2. Add stock to portfolio / update stock shares in portfolio
+      let purchasedStock = await Stock.findOne({
+        where: { userId }
+      })
 
-        // Othewise, purchase x amount of stock shares
-      } else {
-        // 1. Create new transaction
-        const transaction = await Transaction.create({
-          stockSymbol: symbol,
-          pricePerShare: stockData.latestPrice * 100,
+      if (!purchasedStock) {
+        purchasedStock = await Stock.create({
+          symbol,
           shares
         })
-        await user.addTransaction(transaction)
-
-        // 2. Add stock to portfolio / update stock shares in portfolio
-        let purchasedStock = await Stock.findOne({
-          where: { userId }
-        })
-
-        if (!purchasedStock) {
-          purchasedStock = await Stock.create({
-            symbol,
-            shares
-          })
-          await user.addStock(purchasedStock)
-        } else {
-          purchasedStock.shares = purchasedStock.shares + shares
-          await purchasedStock.save()
-        }
-
-        // 3. Update user balance
-        user.balance = user.balance - totalCost
-        await user.save()
-
-        res.status(201).json(transaction)
+        await user.addStock(purchasedStock)
+      } else {
+        purchasedStock.shares = purchasedStock.shares + shares
+        await purchasedStock.save()
       }
+
+      // 3. Update user balance
+      user.balance = user.balance - totalCost
+      await user.save()
+
+      res.status(201).json(transaction)
     }
   } catch (err) {
-    next(err)
+    if (err.response.status === 404) {
+      res.status(404).send(`Invalid ticker symbol: ${symbol}.`)
+    } else {
+      next(err)
+    }
   }
 })
 
