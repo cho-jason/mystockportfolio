5bb9d2bc70ac33eea2d1ab9f75ff87d185fb63a2
Jason Cho <jason.cho.86@gmail.com>
2020-03-06 00:13:17 -0500

    refactor(api): added error handling for invalid ticker symbols for IEX API fetches

--- server/api/transactions.js
+++ server/api/transactions.js
@@ -33,49 +33,57 @@ router.post('/', async (req, res, next) => {
 
     const stockData = await fetch(
       `https://cloud.iexapis.com/stable/stock/${symbol}/quote?token=${API_KEY}`
-    ).then(iexRes => iexRes.json())
-
-    // Note: stockData.latestPrice is in dollars
-    const totalCost = shares * stockData.latestPrice * 100
-    // Note: user.balance is in pennies
-    let user = await User.findByPk(userId)
-
-    // If user has insufficient balance to buy stock shares,
-    // notify user of bad request
-    if (user.balance < totalCost) {
-      res.status(400).send('Insufficient balance.')
-
-      // Othewise, purchase x amount of stock shares
-    } else {
-      // 1. Create new transaction
-      const transaction = await Transaction.create({
-        stockSymbol: symbol,
-        pricePerShare: stockData.latestPrice * 100,
-        shares
-      })
-      await user.addTransaction(transaction)
-
-      // 2. Add stock to portfolio / update stock shares in portfolio
-      let purchasedStock = await Stock.findOne({
-        where: { userId }
+    )
+      .then(iexRes => iexRes.json())
+      .catch(err => {
+        console.log(err.message)
+        console.log(err.stack)
+        res.status(400).send(`Invalid ticker symbol: ${symbol}`)
       })
 
-      if (!purchasedStock) {
-        purchasedStock = await Stock.create({
-          symbol,
+    if (stockData) {
+      // Note: stockData.latestPrice is in dollars
+      const totalCost = shares * stockData.latestPrice * 100
+      // Note: user.balance is in pennies
+      let user = await User.findByPk(userId)
+
+      // If user has insufficient balance to buy stock shares,
+      // notify user of bad request
+      if (user.balance < totalCost) {
+        res.status(400).send('Insufficient balance.')
+
+        // Othewise, purchase x amount of stock shares
+      } else {
+        // 1. Create new transaction
+        const transaction = await Transaction.create({
+          stockSymbol: symbol,
+          pricePerShare: stockData.latestPrice * 100,
           shares
         })
-        await user.addStock(purchasedStock)
-      } else {
-        purchasedStock.shares = purchasedStock.shares + shares
-        await purchasedStock.save()
+        await user.addTransaction(transaction)
+
+        // 2. Add stock to portfolio / update stock shares in portfolio
+        let purchasedStock = await Stock.findOne({
+          where: { userId }
+        })
+
+        if (!purchasedStock) {
+          purchasedStock = await Stock.create({
+            symbol,
+            shares
+          })
+          await user.addStock(purchasedStock)
+        } else {
+          purchasedStock.shares = purchasedStock.shares + shares
+          await purchasedStock.save()
+        }
+
+        // 3. Update user balance
+        user.balance = user.balance - totalCost
+        await user.save()
+
+        res.status(201).json(transaction)
       }
-
-      // 3. Update user balance
-      user.balance = user.balance - totalCost
-      await user.save()
-
-      res.status(201).json(transaction)
     }
   } catch (err) {
     next(err)
