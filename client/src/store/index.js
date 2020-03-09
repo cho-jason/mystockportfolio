eff30b8d9b481cffcb10d87b4429847fc511b1d3
Jason Cho <jason.cho.86@gmail.com>
2020-03-09 00:01:47 -0400

    feat(client): added transactions reducer

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,8 +5,9 @@ import thunk from 'redux-thunk'
 // REDUCERS
 import user from './user'
 import stocks from './stocks'
+import transactions from './transactions'
 
-const reducer = combineReducers({ user, stocks })
+const reducer = combineReducers({ user, stocks, transactions })
 const store = createStore(
   reducer,
   applyMiddleware(thunk, createLogger({ collapsed: true }))
@@ -15,3 +16,4 @@ const store = createStore(
 export default store
 export * from './user'
 export * from './stocks'
+export * from './transactions'
---
+++ client/src/store/transactions.js
@@ -0,0 +1,75 @@
+import axios from 'axios'
+import { addStock, subtractCost } from '.'
+
+// INITIAL STATE
+const defaultTransactions = { list: [], last: {} }
+
+// ACTION TYPES
+const STORE_TRANSACTIONS = 'STORE_TRANSACTIONS'
+const ADD_TRANSACTION = 'ADD_TRANSACTION'
+const STOP_TRANSACTION = 'STOP_TRANSACTION'
+
+// ACTION CREATOR
+const storeTransactions = transactions => ({
+  type: STORE_TRANSACTIONS,
+  transactions
+})
+
+export const addTransaction = transaction => ({
+  type: ADD_TRANSACTION,
+  transaction
+})
+
+export const stopTransaction = error => ({
+  type: STOP_TRANSACTION,
+  error
+})
+
+// THUNK CREATORS
+export const getTransactions = userId => async dispatch => {
+  try {
+    const res = await axios.get(`api/users/${userId}/transactions`)
+    dispatch(storeTransactions(res.data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const purchaseStock = (symbol, shares, userId) => async dispatch => {
+  try {
+    const res = await axios.post(`/api/transactions`, {
+      symbol,
+      shares,
+      userId
+    })
+    const totalCost = res.data.pricePerShare * shares
+    dispatch(addTransaction(res.data))
+    dispatch(addStock(symbol, shares))
+    dispatch(subtractCost(totalCost))
+  } catch (err) {
+    if (err.response.status === 400 || err.response.status === 404) {
+      dispatch(stopTransaction({ errorMessage: err.response.data }))
+    }
+    console.error(err)
+  }
+}
+
+// REDUCER
+const transactionReducer = (state = defaultTransactions, action) => {
+  switch (action.type) {
+    case STORE_TRANSACTIONS:
+      return { ...state, list: action.transactions }
+    case ADD_TRANSACTION:
+      return {
+        ...state,
+        list: [...state.list, action.transaction],
+        last: action.transaction
+      }
+    case STOP_TRANSACTION:
+      return { ...state, last: action.error }
+    default:
+      return state
+  }
+}
+
+export default transactionReducer
